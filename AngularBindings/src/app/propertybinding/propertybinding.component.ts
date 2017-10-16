9df3e00afc2a025fd2acdc1cd193c57beeffab44
Bhuvanesh S <bhuvaneshs@sensiple.com>
2017-10-16 12:11:26 +0530

    initial commit

---
+++ AngularBindings/src/app/propertybinding/propertybinding.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-propertybinding',
+  templateUrl: './propertybinding.component.html',
+  styleUrls: ['./propertybinding.component.css']
+})
+export class PropertybindingComponent {
+
+imageUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+
+}
